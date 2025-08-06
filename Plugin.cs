a317f54594f4b887124c5a20cb453d5f291383e8
drowsiiii <rowenfake@gmail.com>
2025-08-06 16:43:49 +0100

    uploaded wrong files
    empla

--- Plugin.cs
+++ Plugin.cs
@@ -1,47 +1,257 @@
 ﻿using BepInEx;
-using GorillaNetworking;
-using HarmonyLib;
-using System;
-using System.Threading;
 using UnityEngine;
-using Valve.VR;
-
-namespace ForeverHz
+using Utilla;
+using Utilla.Attributes;
+using System;
+using Photon.Pun;
+namespace FixedGTagMod1
 {
-    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    [BepInPlugin("com.drowsii.GravityMesserarounder", "SomethingToDoWithGravity", "1.2.12234443")]
+    [BepInDependency("org.legoandmars.gorillatag.utilla")]
+    [ModdedGamemode] // Enable callbacks in default modded gamemodes
     public class Plugin : BaseUnityPlugin
     {
-        public static Plugin instance;
-        public int HZ = 60;
-        public bool gripOnly = false;
-        void Start() =>
-            instance = this;
+        private bool inAllowedRoom = false;
+        private bool buttonrandom;
+        private bool buttonnograv;
+        private bool buttonreset;
+        private bool buttony;
+        private bool buttonx;
+        private bool buttonz;
+        private bool buttonconstantrandom;
+        private bool minusbuttonx;
+        private bool minusbuttonz;
+        private bool buttonreset2;
+        private bool Lowgrav;
+
+        private bool HighGrav;
+        private bool Disconnecter = true; // Safety Disconnect Spam
 
         void OnGUI()
         {
-            HZ = (int)GUI.HorizontalSlider(new Rect(4f, 4f, 400f, 40f), HZ, 0f, 150f);
-            GUI.Label(new Rect(414f, 1f, 144f, 60f), "HZ: " + HZ.ToString());
-            GUI.Label(new Rect(4f, 40f, 900f, 40f), "HZ slider by @goldentrophy");
-            gripOnly = GUI.Toggle(new Rect(300f, 40f, 1080f, 40f), gripOnly, "Right Grip");
+            // GUI SPACINGS: Text always 50 away from a button or another GUI element, and a away from the side of the screen.    The buttons are 20 away from eachother.
+
+
+            int x = 230;
+            int y = 600;
+
+            GUI.color = Color.magenta;
+            GUI.Label(new Rect(x, y, 600, 600), "@drowsiiis gravity stinky thing - made with confusion for no reason <3");
+
+            y += 50; // space below label
+            GUI.color = Color.yellow;
+            GUI.Label(new Rect(x, y, 600, 600), "no clue if you will get banned with this, so... your at fault if you get struck by the ban hammer, You can use this mod in disconnected rooms (not in room) ");
+
+            y += 65;
+            GUI.Label(new Rect(x, y, 600, 600), "i am not responsible for any bans or anything that happens to you, so dont blame me if you get banned ive added a always disconnect button for you so you can use the mod safely");
+
+            y += 80;
+            GUI.color = Color.cyan;
+            this.Disconnecter = GUI.Toggle(new Rect(x, y, 200, 20), Disconnecter, "Safety Disconnect Spam ");
+
+            y += 50;
+            GUI.color = Color.white;
+            GUI.Label(new Rect(x, y, 600, 600), "Gravity Things:");
+
+            y += 50;
+            GUI.color = Color.cyan;
+            this.buttonrandom = GUI.Button(new Rect(x, y, 200, 20), "Randomise Gravity (i love this)");
+
+            y += 30;
+            GUI.color = Color.grey;
+            this.buttonnograv = GUI.Button(new Rect(x, y, 200, 20), "No Gravity");
+
+            y += 30;
+            GUI.color = Color.white;
+            this.buttonreset = GUI.Button(new Rect(x, y, 200, 20), "Reset/Normal Gravity");
+
+            y += 50;
+            GUI.color = Color.white;
+            GUI.Label(new Rect(x, y, 600, 600), "Axis Gravity Things:");
+
+            y += 50;
+            GUI.color = Color.green;
+            this.buttony = GUI.Button(new Rect(x, y, 200, 20), "Y axis Grav (Upwards Gravity)");
+            y += 30;
+            GUI.color = Color.cyan;
+            this.buttonx = GUI.Button(new Rect(x, y, 200, 20), "X axis Grav");
+            y += 30;
+            GUI.color = Color.red;
+            this.buttonz = GUI.Button(new Rect(x, y, 200, 20), "Z axis Grav");
+
+
+            y += 50;
+            GUI.color = Color.green;
+            this.buttonreset2 = GUI.Button(new Rect(x, y, 200, 20), "-Y axis Grav (Downwards Gravity)");
+            y += 30;
+            GUI.color = Color.cyan;
+            this.minusbuttonx = GUI.Button(new Rect(x, y, 200, 20), "-X axis Grav");
+            y += 30;
+            GUI.color = Color.red;
+            this.minusbuttonz = GUI.Button(new Rect(x, y, 200, 20), "-Z axis Grav");
+
+
+            y += 50;
+            GUI.color = Color.white;
+            GUI.Label(new Rect(x, y, 600, 600), "Stupid Gravity Things:");
+
+            y += 50;
+            GUI.color = Color.cyan;
+            this.buttonconstantrandom = GUI.Toggle(new Rect(x, y, 200, 20), buttonconstantrandom, "(Really Funny) Random Spaz Gravity");
+            y += 30;
+            GUI.color = Color.yellow;
+            this.Lowgrav = GUI.Button(new Rect(x, y, 200, 20), "Low Gravity");
+            y += 30;
+            GUI.color = Color.yellow;
+            this.HighGrav = GUI.Button(new Rect(x, y, 200, 20), "High Gravity");
+
+
         }
 
-        private static float lastTime;
-        void Update()
+        void RandomGravity()
         {
-            if (gripOnly && !ControllerInputPoller.instance.rightGrab)
-                return;
+            System.Random random = new System.Random();
+            float Val1 = random.Next(-100, 100);
+            float Val2 = random.Next(-100, 100);
+            float Val3 = random.Next(-100, 100);
+            Physics.gravity = new Vector3(Val1, Val2, Val3);
+        }
 
-            float targetDelta = 1f / HZ;
-            float elapsed = Time.realtimeSinceStartup - lastTime;
+        void NoGravity()
+        {
+            Physics.gravity = new Vector3(0, 0, 0);
+        }
 
-            if (elapsed < targetDelta)
+        void ResetGravity()
+        {
+            Physics.gravity = new Vector3(0, -9.81f, 0);
+        }
+
+        void LowGravity()
+        {
+            Physics.gravity = new Vector3(0, -2.54f, 0);
+        }
+        void HighGravity()
+        {
+            Physics.gravity = new Vector3(0, -18.26f, 0);
+        }
+
+        void YAxisGravity()
+        {
+            Physics.gravity = new Vector3(0, 9.81f, 0);
+        }
+
+        void XAxisGravity()
+        {
+            Physics.gravity = new Vector3(9.81f, 0, 0);
+        }
+
+        void ZAxisGravity()
+        {
+            Physics.gravity = new Vector3(0, 0, 9.81f);
+        }
+
+        void MinusXAxisGravity()
+        {
+            Physics.gravity = new Vector3(-9.81f, 0, 0);
+        }
+
+        void MinusZAxisGravity()
+        {
+            Physics.gravity = new Vector3(0, 0, -9.81f);
+        }
+        void disconnecter()
+        {
+            if (Disconnecter && PhotonNetwork.IsConnected)
             {
-                int sleepMs = Mathf.FloorToInt((targetDelta - elapsed) * 1000);
-                if (sleepMs > 0)
-                    Thread.Sleep(sleepMs);
+                PhotonNetwork.Disconnect();
             }
+        }
 
-            lastTime = Time.realtimeSinceStartup;
+        private void Update()
+        {
+
+            disconnecter();
+            if (inAllowedRoom || !PhotonNetwork.IsConnected)
+            {
+                if (HighGrav)
+                {
+                    HighGravity();
+                    HighGrav = false;
+                }
+                if (Lowgrav)
+                {
+                    LowGravity();
+                    Lowgrav = false;
+                }
+                if (buttonrandom)
+                {
+                    RandomGravity();
+                    buttonrandom = false;
+                }
+                if (buttonnograv)
+                {
+                    NoGravity();
+                    buttonnograv = false;
+                }
+                if (buttonreset)
+                {
+                    ResetGravity();
+                    buttonreset = false;
+                }
+                if (buttonreset2)
+                {
+                    ResetGravity();
+                    buttonreset2 = false;
+                }
+                if (buttony)
+                {
+                    YAxisGravity();
+                    buttony = false;
+                }
+                if (buttonx)
+                {
+                    XAxisGravity();
+                    buttonx = false;
+                }
+                if (buttonz)
+                {
+                    ZAxisGravity();
+                    buttonz = false;
+                }
+                if (buttonconstantrandom)
+                {
+                    RandomGravity();
+                }
+                if (minusbuttonx)
+                {
+                    MinusXAxisGravity();
+                    minusbuttonx = false;
+                }
+                if (minusbuttonz)
+                {
+                    MinusZAxisGravity();
+                    minusbuttonz = false;
+                }
+
+            }
+            else
+            {
+                Physics.gravity = new Vector3(0, -9.81f, 0); 
+            }
+        }
+
+        [ModdedGamemodeJoin]
+        private void RoomJoined(string gamemode)
+        {
+            inAllowedRoom = true;
+        }
+
+        [ModdedGamemodeLeave]
+        private void RoomLeft(string gamemode)
+        {
+            inAllowedRoom = false;
         }
     }
 }
+
--- PluginInfo.cs
+++ PluginInfo.cs
@@ -5,8 +5,8 @@
     /// </summary>
     internal class PluginInfo
     {
-        public const string GUID = "com.goldentrophy.gorillatag.foreverhz";
-        public const string Name = "ForeverHz";
-        public const string Version = "1.0.0";
+        public const string GUID = "com.@drowsiii.GravityMod";
+        public const string Name = "GravityMod";
+        public const string Version = "1.2.1";
     }
 }
